2d91b4d8d016d125e39b52619d731f215715d897
Dzikri Nursyaban <dzikrin022@gmail.com>
2023-11-17 21:25:07 +0700

    Create shell script for build and push image

---
+++ build_push_image.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Build docker image
+docker build -t item-app:v1 .;
+
+# Show list images
+docker images ls;
+
+# Change image name for github packages
+docker tag item-app:v1 ghcr.io/mrdzick/item-app:v1;
+
+# Set personal access token of github
+export CR_PAT=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;
+
+# Login to container registry
+echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin;
+
+# Push docker image to container registry
+docker push ghcr.io/mrdzick/item-app:v1;
